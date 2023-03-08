8897c2781e785984960bbf412edfba46c550a25f
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-03-09 04:54:49 +0530

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -1,2 +1,36 @@
-enter = input(":")
-print(enter)
+# define the calculator function
+
+def calculator():
+
+    number1 = int(input("Enter first number: ")
+
+    number2 = int(input("Enter second number: "))
+
+    operator = input("Enter operator (+, -, *, /): ")
+
+    
+
+    if operator == '+':
+
+        result = number1 + number2
+
+    elif operator == '-':
+
+        result = number1 - number2
+
+    elif operator == '*':
+
+        result = number1 * number2
+
+    elif operator == '/':
+
+        result = number1 / number2
+
+    else:
+
+        print("Invalid operator")
+
+    print("Result: ", result)
+if __name__ == "__main__":
+
+    calculator()
